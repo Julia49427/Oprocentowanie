bee65b85b98fcc8da540cb6ee93560d07f89c1ca
Julia <patyczak49427@wp.pl>
2021-12-15 15:00:27 +0100

    Wklejam calosc kodu

---
+++ oprocentowanie1.py
@@ -0,0 +1,146 @@
+infl1 = float(1.593)
+infl2 = float(-0.454)
+infl3 = float(2.325)
+infl4 = float(1.261)
+infl5 = float(1.783)
+infl6 = float(2.329)
+infl7 = float(1.502)
+infl8 = float(1.783)
+infl9 = float(2.329)
+infl10 = float(0.617)
+infl11 = float(2.352)
+infl12 = float(0.338)
+infl13 = float(1.577)
+infl14 = float(-0.293)
+infl15 = float(2.486)
+infl16 = float(0.267)
+infl17 = float(1.418)
+infl18 = float(1.054)
+infl19 = float(1.481)
+infl20 = float(1.577)
+infl21 = float(-0.077)
+infl22 = float(1.166)
+infl23 = float(-0.404)
+infl24 = float(1.500)
+
+kwotapocz = int(12000)
+rata = int(200)
+
+zostanie1 = float((1 + ((infl1 + 3) / 1200)) * kwotapocz - rata)
+print("Styczeń")
+print(f"Twoja pozostała kwota kredytu to {zostanie1}.")
+
+zostanie2 = float((1 + ((infl2 + 3) / 1200)) * zostanie1 - rata)
+mniej2 = zostanie1 - zostanie2
+print("Luty")
+print(f"Twoja pozostała kwota kredytu to {zostanie2}, to  {mniej2} mniej niż w poprzednim miesiącu.")
+
+zostanie3 = float((1 + ((infl3 + 3) / 1200)) * zostanie2 - rata)
+mniej3 = zostanie2 - zostanie3
+print("Marzec")
+print(f"Twoja pozostała kwota kredytu to {zostanie3}, to  {mniej3} mniej niż w poprzednim miesiącu.")
+
+zostanie4 = float((1 + ((infl4 + 3) / 1200)) * zostanie3 - rata)
+mniej4 = zostanie3 - zostanie4
+print("Kwiecień")
+print(f"Twoja pozostała kwota kredytu to {zostanie4}, to  {mniej4} mniej niż w poprzednim miesiącu.")
+
+zostanie5 = float((1 + ((infl5 + 3) / 1200)) * zostanie4 - rata)
+mniej5 = zostanie4 - zostanie5
+print("Maj")
+print(f"Twoja pozostała kwota kredytu to {zostanie5}, to  {mniej5} mniej niż w poprzednim miesiącu.")
+
+zostanie6 = float((1 + ((infl6 + 3) / 1200)) * zostanie5 - rata)
+mniej6 = zostanie5 - zostanie6
+print("Czerwiec")
+print(f"Twoja pozostała kwota kredytu to {zostanie6}, to  {mniej6} mniej niż w poprzednim miesiącu.")
+
+zostanie7 = float((1 + ((infl7 + 3) / 1200)) * zostanie6 - rata)
+mniej7 = zostanie6 - zostanie7
+print("Lipiec")
+print(f"Twoja pozostała kwota kredytu to {zostanie7}, to  {mniej7} mniej niż w poprzednim miesiącu.")
+
+zostanie8 = float((1 + ((infl8 + 3) / 1200)) * zostanie7 - rata)
+mniej8 = zostanie7 - zostanie8
+print("Sierpień")
+print(f"Twoja pozostała kwota kredytu to {zostanie8}, to  {mniej8} mniej niż w poprzednim miesiącu.")
+
+zostanie9 = float((1 + ((infl9 + 3) / 1200)) * zostanie8 - rata)
+mniej9 = zostanie8 - zostanie9
+print("Wrzesień")
+print(f"Twoja pozostała kwota kredytu to {zostanie9}, to  {mniej9} mniej niż w poprzednim miesiącu.")
+
+zostanie10 = float((1 + ((infl10 + 3) / 1200)) * zostanie9 - rata)
+mniej10 = zostanie9 - zostanie10
+print("Październik")
+print(f"Twoja pozostała kwota kredytu to {zostanie10}, to  {mniej10} mniej niż w poprzednim miesiącu.")
+
+zostanie11 = float((1 + ((infl11 + 3) / 1200)) * zostanie10 - rata)
+mniej11 = zostanie10 - zostanie11
+print("Listopad")
+print(f"Twoja pozostała kwota kredytu to {zostanie11}, to  {mniej11} mniej niż w poprzednim miesiącu.")
+
+zostanie12 = float((1 + ((infl12 + 3) / 1200)) * zostanie11 - rata)
+mniej12 = zostanie11 - zostanie12
+print("Grudzień")
+print(f"Twoja pozostała kwota kredytu to {zostanie12}, to  {mniej12} mniej niż w poprzednim miesiącu.")
+
+zostanie13 = float((1 + ((infl13 + 3) / 1200)) * zostanie12 - rata)
+mniej13 = zostanie12 - zostanie13
+print("Styczeń")
+print(f"Twoja pozostała kwota kredytu to {zostanie13}, to  {mniej13} mniej niż w poprzednim miesiącu.")
+
+zostanie14 = float((1 + ((infl14 + 3) / 1200)) * zostanie13 - rata)
+mniej14 = zostanie13 - zostanie14
+print("Luty")
+print(f"Twoja pozostała kwota kredytu to {zostanie14}, to  {mniej14} mniej niż w poprzednim miesiącu.")
+
+zostanie15 = float((1 + ((infl15 + 3) / 1200)) * zostanie14 - rata)
+mniej15 = zostanie14 - zostanie15
+print("Marzec")
+print(f"Twoja pozostała kwota kredytu to {zostanie15}, to  {mniej15} mniej niż w poprzednim miesiącu.")
+
+zostanie16 = float((1 + ((infl16 + 3) / 1200)) * zostanie15 - rata)
+mniej16 = zostanie15 - zostanie16
+print("Kwiecień")
+print(f"Twoja pozostała kwota kredytu to {zostanie16}, to  {mniej16} mniej niż w poprzednim miesiącu.")
+
+zostanie17 = float((1 + ((infl17 + 3) / 1200)) * zostanie16 - rata)
+mniej17 = zostanie16 - zostanie17
+print("Maj")
+print(f"Twoja pozostała kwota kredytu to {zostanie17}, to  {mniej17} mniej niż w poprzednim miesiącu.")
+
+zostanie18 = float((1 + ((infl18 + 3) / 1200)) * zostanie17 - rata)
+mniej18 = zostanie17 - zostanie18
+print("Czerwiec")
+print(f"Twoja pozostała kwota kredytu to {zostanie18}, to  {mniej18} mniej niż w poprzednim miesiącu.")
+
+zostanie19 = float((1 + ((infl19 + 3) / 1200)) * zostanie18 - rata)
+mniej19 = zostanie18 - zostanie19
+print("Lipiec")
+print(f"Twoja pozostała kwota kredytu to {zostanie19}, to  {mniej19} mniej niż w poprzednim miesiącu.")
+
+zostanie20 = float((1 + ((infl20 + 3) / 1200)) * zostanie19 - rata)
+mniej20 = zostanie19 - zostanie20
+print("Sierpień")
+print(f"Twoja pozostała kwota kredytu to {zostanie19}, to  {mniej19} mniej niż w poprzednim miesiącu.")
+
+zostanie21 = float((1 + ((infl21 + 3) / 1200)) * zostanie20 - rata)
+mniej21 = zostanie20 - zostanie21
+print("Wrzesień")
+print(f"Twoja pozostała kwota kredytu to {zostanie21}, to  {mniej21} mniej niż w poprzednim miesiącu.")
+
+zostanie22 = float((1 + ((infl22 + 3) / 1200)) * zostanie21 - rata)
+mniej22 = zostanie21 - zostanie22
+print("Październik")
+print(f"Twoja pozostała kwota kredytu to {zostanie22}, to  {mniej22} mniej niż w poprzednim miesiącu.")
+
+zostanie23 = float((1 + ((infl23 + 3) / 1200)) * zostanie22 - rata)
+mniej23 = zostanie22 - zostanie23
+print("Listopad")
+print(f"Twoja pozostała kwota kredytu to {zostanie23}, to  {mniej23} mniej niż w poprzednim miesiącu.")
+
+zostanie24 = float((1 + ((infl24 + 3) / 1200)) * zostanie23 - rata)
+mniej24 = zostanie23 - zostanie24
+print("Grudzień")
+print(f"Twoja pozostała kwota kredytu to {zostanie24}, to  {mniej24} mniej niż w poprzednim miesiącu.")
